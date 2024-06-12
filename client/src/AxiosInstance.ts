d65c3f51da78f385b5dbd555a98a5f9ac7b1992a
Nithit Lertcharoensombat <xXNeonKitsuneXx@gmail.com>
2024-06-13 01:19:18 +0700

    fix(API): At Axios

--- client/src/AxiosInstance.ts
+++ client/src/AxiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Axios = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL ?? "http://localhost:8888",
+  baseURL: import.meta.env.VITE_BACKEND_URL ?? "http://localhost:8888/api",
 });
 
 export { Axios };
