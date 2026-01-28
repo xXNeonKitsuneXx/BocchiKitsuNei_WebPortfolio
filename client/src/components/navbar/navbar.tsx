7e460af12ad08c2e5876bbe34d0ad6f583f85599
Nithit Lertcharoensombat <xXNeonKitsuneXx@gmail.com>
2026-01-28 22:22:32 +0700

    fix nav

--- client/src/components/navbar/navbar.tsx
+++ client/src/components/navbar/navbar.tsx
@@ -3,24 +3,24 @@ import { Button } from "../ui/button";
 export const Navbar = () => {
   return (
     <nav
-      className="fixed top-0 p-2 flex justify-center mt-6 items-center bg-black z-20"
+      className="fixed top-6 left-0 right-0 p-2 flex justify-center mt-6 items-center bg-black z-20"
       style={{ borderRadius: '9999px', width: 'max-content' }}
       data-aos="fade-down"
     >
       <div className="flex justify-center px-4">
         <ul className="flex space-x-8 text-white">
           <li>
-            <Button className="text-white font-medium hover:text-purple-400 transition-colors">
+            <Button className="text-white font-medium hover:text-purple-400 transition-colors bg-black">
               <a href="/">Home</a>
             </Button>
           </li>
           <li>
-            <Button className="text-white font-medium hover:text-purple-400 transition-colors">
+            <Button className="text-white font-medium hover:text-purple-400 transition-colors bg-black">
               <a href="/project">Project</a>
             </Button>
           </li>
           <li>
-            <Button className="text-white font-medium hover:text-purple-400 transition-colors">
+            <Button className="text-white font-medium hover:text-purple-400 transition-colors bg-black">
               <a href="/contact">Contact</a>
             </Button>
           </li>
