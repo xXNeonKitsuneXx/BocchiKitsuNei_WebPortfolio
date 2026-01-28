5cc098fac2252f03c4a5efb739d1894c162015ca
Nithit Lertcharoensombat <xXNeonKitsuneXx@gmail.com>
2026-01-28 23:16:54 +0700

    Update hero.tsx

--- client/src/components/homepage/hero.tsx
+++ client/src/components/homepage/hero.tsx
@@ -6,17 +6,17 @@ export const Hero = () => {
       <div className="flex items-center justify-center">
         <Navbar />
       </div>
-      <div className="relative w-full h-[90vh] flex items-end md:items-center">
+      <div className="relative w-full flex-1 flex items-end md:items-center">
         <img
           src="https://minio.bocchikitsunei.com/bocchikitsuneiwebportfolio/Nei_PR_R_MH_CutOut.png"
           alt="Profile photo of BocchiKitsunei"
           className="absolute inset-0 w-full h-full object-cover object-right"
         />
-        <div className="relative z-10 px-12 max-w-3xl">
-          <h1 className="text-5xl md:text-7xl lg:text-8xl font-bold text-purple-500 leading-tight">
+        <div className="relative z-10 md:px-12 max-w-3xl">
+          <h1 className="text-4xl md:text-7xl lg:text-8xl font-bold text-purple-500 leading-tight">
             BocchiKitsunei
           </h1>
-          <p className="text-white md:text-black pb-3 text-xl md:text-2xl font-semibold">
+          <p className="text-white md:text-black pb-3 text-lg md:text-2xl font-semibold">
             Nithit Lertcharoensombat{" "}
             <span className="font-extrabold text-purple-500">|</span> Software
             Engineer
