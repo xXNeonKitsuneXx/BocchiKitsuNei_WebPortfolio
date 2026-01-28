838b79839efa8b073c2cdfb9696c14033e942a42
Nithit Lertcharoensombat <xXNeonKitsuneXx@gmail.com>
2026-01-29 06:52:39 +0700

    fix size change

--- client/src/components/homepage/hero.tsx
+++ client/src/components/homepage/hero.tsx
@@ -13,10 +13,10 @@ export const Hero = () => {
           className="absolute inset-0 w-full h-[634px] md:h-[934px] object-cover object-right"
         />
         <div className="relative z-10 px-4 md:px-12 max-w-3xl">
-          <h1 className="text-4xl md:text-7xl lg:text-8xl font-bold text-purple-500 leading-tight">
+          <h1 className="text-4xl md:text-6xl lg:text-7xl font-bold text-purple-500 leading-tight">
             Nithit Lertcharoensombat
           </h1>
-          <p className="text-white md:text-black pb-3 text-lg md:text-2xl font-semibold">
+          <p className="text-white md:text-black pb-3 text-lg md:text-3xl font-semibold">
             Software Engineer
           </p>
         </div>
