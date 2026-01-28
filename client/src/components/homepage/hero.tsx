78abf98ae7455ad8424ca411124f9bf21bcb57a4
Nithit Lertcharoensombat <xXNeonKitsuneXx@gmail.com>
2026-01-28 22:02:48 +0700

    Update hero.tsx

--- client/src/components/homepage/hero.tsx
+++ client/src/components/homepage/hero.tsx
@@ -4,8 +4,8 @@ export const Hero = () => {
   return (
     <section className="flex justify-center items-center">
       <Navbar />
-      <div className="relative w-full h-[80vh] flex items-center justify-center" data-aos="zoom-in">
-        <img src="https://minio.bocchikitsunei.com/bocchikitsuneiwebportfolio/Nei_PR_R.png" alt="Background Image" className="absolute inset-0 w-full h-full object-cover" />
+      <div className="relative w-full h-[80vh] flex items-start justify-items-start" data-aos="zoom-in">
+        <img src="https://minio.bocchikitsunei.com/bocchikitsuneiwebportfolio/Nei_PR_R_MH.png" alt="Background Image" className="absolute inset-0 w-full h-full object-cover" />
         <div className="relative z-10 text-purple-500">
           <h1 className="text-4xl md:text-6xl font-bold" data-aos="fade-up">BocchiKitsunei</h1>
           <p className="text-white mt-2 text-lg md:text-xl px-10 font-semibold text-center" data-aos="fade-up">Nithit Lertcharoensombat <span className="font-extrabold"> I </span> Sofeware Developer</p>
