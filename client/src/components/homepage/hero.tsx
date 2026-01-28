afaf0a128fe71d27756aaaf60933b0aaed5e55e5
Nithit Lertcharoensombat <xXNeonKitsuneXx@gmail.com>
2026-01-28 22:37:29 +0700

    final v1

--- client/src/components/homepage/hero.tsx
+++ client/src/components/homepage/hero.tsx
@@ -4,20 +4,20 @@ export const Hero = () => {
   return (
     <section className="relative w-full min-h-screen">
       <Navbar />
-      <div className="relative w-full h-[90vh] flex items-center">
+      <div className="relative w-full h-[90vh] flex items-end pb-12 md:items-center md:pb-0">
         <img
-          src="https://minio.bocchikitsunei.com/bocchikitsuneiwebportfolio/Nei_PR_R_MH.png"
+          src="https://minio.bocchikitsunei.com/bocchikitsuneiwebportfolio/Nei_PR_R_MH_CutOut.png"
           alt="Profile photo of BocchiKitsunei"
           className="absolute inset-0 w-full h-full object-cover object-right"
         />
-        <div className="relative z-10 px-8 md:px-16 lg:px-24 max-w-2xl">
+        <div className="relative z-10 px-8 md:px-16 lg:px-24 max-w-3xl">
           <h1 className="text-5xl md:text-7xl lg:text-8xl font-bold text-purple-500 leading-tight">
             BocchiKitsunei
           </h1>
           <p className="text-black mt-4 text-xl md:text-2xl font-semibold">
             Nithit Lertcharoensombat{" "}
             <span className="font-extrabold text-purple-500">|</span> Software
-            Developer
+            Engineer
           </p>
         </div>
       </div>
