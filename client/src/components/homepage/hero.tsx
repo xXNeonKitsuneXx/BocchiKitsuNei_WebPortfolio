720d120660b42de8d64cecb5b73b7b4d7148c610
Nithit Lertcharoensombat <xXNeonKitsuneXx@gmail.com>
2026-01-29 00:36:45 +0700

    Update hero.tsx

--- client/src/components/homepage/hero.tsx
+++ client/src/components/homepage/hero.tsx
@@ -6,11 +6,11 @@ export const Hero = () => {
       <div className="flex items-center justify-center"> 
         <Navbar />
       </div>
-      <div className="relative w-full h-[934px] flex items-end md:items-center">
+      <div className="relative w-full h-[634px] md:h-[934px] flex items-end justify-center md:justify-normal md:items-center">
         <img
           src="https://minio.bocchikitsunei.com/bocchikitsuneiwebportfolio/Nei_Hero.jpg"
           alt="Profile photo of BocchiKitsunei"
-          className="absolute inset-0 w-full h-[934px] object-cover object-right"
+          className="absolute inset-0 w-full h-[634px] md:h-[934px] object-cover object-right"
         />
         <div className="relative z-10 px-4 md:px-12 max-w-3xl">
           <h1 className="text-4xl md:text-7xl lg:text-8xl font-bold text-purple-500 leading-tight">
